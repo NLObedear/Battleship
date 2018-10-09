1dcac28473ef5bd34db8588510ceba0a1c719f34
xeyess <42161833+xeyess@users.noreply.github.com>
2018-10-09 13:01:10 +1100

    Extended high-score feature
    - Allows the high-score name input to accept 10 characters instead of 3.

--- HighScoreController.cs
+++ HighScoreController.cs
@@ -16,7 +16,7 @@ using SwinGameSDK;
 /// </remarks>
 static class HighScoreController
 {
-	private const int NAME_WIDTH = 3;
+	private const int NAME_WIDTH = 10; //*EDIT
 
 	private const int SCORES_LEFT = 490;
 	/// <summary>
@@ -193,8 +193,8 @@ static class HighScoreController
 
 			s.Name = SwinGame.TextReadAsASCII();
 
-			if (s.Name.Length < 3) {
-				s.Name = s.Name + new string(Convert.ToChar(" "), 3 - s.Name.Length);
+			if (s.Name.Length < NAME_WIDTH) { //*EDIT
+				s.Name = s.Name + new string(Convert.ToChar(" "), NAME_WIDTH - s.Name.Length);
 			}
 
 			_Scores.RemoveAt(_Scores.Count - 1);
